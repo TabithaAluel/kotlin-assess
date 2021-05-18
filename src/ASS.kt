337e311c80d2825598a4f2ddd81fb45067f0f2dd
TabithaAluel <alueltabitha@gmail.com>
2021-05-18 18:02:03 +0300

    work

---
+++ src/ASS.kt
@@ -0,0 +1,38 @@
+fun main() {
+   println(bookLists("River the source","The Pearl","Memories we lost"))
+load(12)
+   unload(10)
+   weightcheck()
+
+
+}
+data class Book(var title:String,var author:String,var pages:String){
+
+}
+
+fun bookLists(book1:String,book2:String,book3:String){
+   var x= mutableListOf<Book>()
+
+
+   }
+//fun squares(x>300){
+//   while (x)
+//}
+
+
+ data class Tank(val registration:String,val totalWeight:Int,val capacity:Float){
+
+ }
+
+
+fun load(weight:Int){
+    weight=
+
+}
+fun unload(weight: Int){
+   totalWeight--
+
+}
+fun weightcheck(){
+
+}
